bf06dee3304e6531582513131ac404740d256489
Agilefox <brian.s.cooper@gmail.com>
2022-12-07 18:00:03 -0500

    overengineered and done with it

--- 2022/07-No-Space-Left-On-Device/part1.go
+++ 2022/07-No-Space-Left-On-Device/part1.go
@@ -63,25 +63,44 @@ func newFolder(name string, parent *Folder) *Folder {
 	return &Folder{name, []File{}, make(map[string]*Folder), parent}
 }
 
-func findCandidates(pointer *Folder, dirs []string) (int, []string) {
-	log.Println(pointer.Name, len(pointer.Files))
-	size := 0
-	for _, folder := range pointer.Folders {
-		log.Println(folder.Name)
-		s, d := findCandidates(folder, dirs)
-		size += s
-		dirs = append(dirs, d...)
+func getFullPath(dir *Folder) string {
+	path := dir.Name
+	if dir.ParentFolder == nil {
+		return path
 	}
+	for p := dir.ParentFolder; p.ParentFolder != nil; p = p.ParentFolder {
+		path = fmt.Sprintf("%s/%s", p.Name, path)
+	}
+	return path
+}
 
-	for _, file := range pointer.Files {
+var sum = 0
+
+func findCandidates(cur_dir *Folder) int {
+	size := 0
+
+	// count all the files in the directory
+	for _, file := range cur_dir.Files {
 		size += file.Size
 	}
+	sum += size
+	// log.Println(fmt.Sprintf("%s->%d", cur_dir.Name, size))
 
-	if size <= 100000 {
-		dirs = append(dirs, pointer.Name)
+	// go down into the sub directory
+	for _, folder := range cur_dir.Folders {
+		size += findCandidates(folder)
 	}
 
-	return size, dirs
+	// log.Println(getFullPath(cur_dir))
+	// log.Println("\t", size, size > 27322861)
+	if size >= 2677139 {
+		log.Println(size, cur_dir.Name, size, "*******************")
+	}
+	// if size >= 100000 {
+	// 	log.Println(cur_dir.Name, size)
+	// 	sum += size
+	// }
+	return size
 
 }
 
@@ -94,38 +113,41 @@ func main() {
 	// skip the first line, it goes to root
 	lines = lines[1:]
 	root := Folder{"/", []File{}, make(map[string]*Folder), nil}
-	pointer := &root
+	cur_dir := &root
 
 	// build directory tree
 	for _, line := range lines {
 		// log.Println(root)
-		// log.Println(num, line, pointer.Name)
-		if line == "cd .." {
-			pointer = pointer.ParentFolder
+		// log.Println(num, line, cur_dir.Name)
+		if line == "$ cd .." {
+			cur_dir = cur_dir.ParentFolder
+			// log.Println("Now in ", cur_dir.Name)
 		} else if strings.HasPrefix(line, "$ cd") {
-			// log.Println(pointer.Folders, line[4:])
-			for k, f := range pointer.Folders {
-				if k == line[4:] {
-					pointer = f
-					break
-				}
-			}
-			// log.Println(pointer.Name)
+			name := line[5:]
+			cur_dir = cur_dir.Folders[name]
 		} else if strings.HasPrefix(line, "dir ") {
 			name := line[4:]
-			pointer.Folders[name] = newFolder(name, pointer)
-			// log.Println(pointer.Folders[line[4:]], pointer.Folders[line[4:]].Name, pointer.Folders[line[4:]].ParentFolder.Name)
+			// log.Println(fmt.Sprintf("Add %s to %s", name, getFullPath(cur_dir)))
+			cur_dir.Folders[name] = newFolder(name, cur_dir)
+
+			// log.Println(n+2, "Created dir", getFullPath(cur_dir.Folders[name]))
+			// log.Println(cur_dir.Folders[line[4:]], cur_dir.Folders[line[4:]].Name, cur_dir.Folders[line[4:]].ParentFolder.Name)
 		} else if !strings.HasPrefix(line, "$ ls") {
 			s := strings.Split(line, " ")
 			size, _ := strconv.Atoi(s[0])
 			name := s[1]
-			pointer.Files = append(pointer.Files, File{name, size})
+			cur_dir.Files = append(cur_dir.Files, File{name, size})
 		}
 	}
-	for _, f := range root.Folders {
-		log.Println(f.Name, len(f.Files), len(f.Folders))
-	}
+	// for _, f := range root.Folders {
+	// 	log.Println(f.Name, len(f.Files), len(f.Folders))
+	// }
 	// walk directory tree
-	// var dirs []string
-	// log.Println(findCandidates(&root, dirs))
+
+	// log.Println(findCandidates(&root), sum)
+	size := findCandidates(&root)
+	log.Println(size, sum)
 }
+
+// 70,000,000 - 42,677,139 = 27,322,861
+// 30000000 - 27322861 = 2677139
